6d2af3753b40d64c023dc6f94ee31245f7a6bb84
SarvarBek <ssarvar507@gmail.com>
2025-04-03 11:02:53 +0500

    add fav view contoller and update main tab bar controller

---
+++ MyRecipes/Domain/Scenes/FavouriteRecipes/FavouritesViewController.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  MyRecipes
+//
+//  Created by Sarvar Boltaboyev on 03/04/25.
+//
+
+import Foundation
+import UIKit
+
+class FavouritesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func style() {
+        
+    }
+    
+    func layout() {
+        
+    }
+    
+}
--- MyRecipes/MainTabBarController.swift
+++ MyRecipes/MainTabBarController.swift
@@ -8,23 +8,45 @@
 import UIKit
 
 class MainTabBarController: UITabBarController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        tabBar.tintColor = .systemMint
         setupViewController()
+        set()
     }
-
-    func setupViewController() {
     
+    func setupViewController() {
+        
         let recipesViewController = RecipesViewController.configure()
-        // TODO: FAVOURITE VIEW 
+        let favViewController = FavouritesViewController()
         
         let recipesNavController = UINavigationController(rootViewController: recipesViewController)
+        let favNavConrtoller = UINavigationController(rootViewController: favViewController)
         
-        recipesNavController.tabBarItem =  UITabBarItem(title: "Recipes", image: UIImage(systemName: "book"), tag: 0)
-                
-        viewControllers = [recipesNavController]
+        recipesNavController.tabBarItem =  UITabBarItem(title: "Recipes",
+                                                        image: UIImage(systemName: "book"),
+                                                        tag: 0)
+        
+        favNavConrtoller.tabBarItem = UITabBarItem(title: "Favourites",
+                                                   image: UIImage(systemName: "heart"),
+                                                   tag: 1)
+        
+        viewControllers = [recipesNavController, favNavConrtoller]
+    }
+    
+    func set() {
+        if let favTabItem = tabBar.items?[1] {
+            favTabItem.image = UIImage(systemName: "heart")
+            favTabItem.selectedImage = UIImage(systemName: "heart.fill")
+        }
+        
+        
+        if let recipeTabItem = tabBar.items?[0] {
+            recipeTabItem.image  = UIImage(systemName: "book")
+            recipeTabItem.selectedImage = UIImage(systemName: "book.fill")
+        }
     }
 }
 
